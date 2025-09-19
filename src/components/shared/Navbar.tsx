f6f5907082a0fbfc2507c67ed3752a9fb65ccac3
azeem <azeem@ak.com>
2025-09-19 21:03:13 +0530

    resoveled bug cart for mobile

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -148,7 +148,7 @@ const Navbar = () => {
                 }
                 `}
                 to={"/cart"}
-                onClick={() => setNavbarOpen(!navbarOpen)}
+                onClick={() => setNavbarOpen(false)}
               >
                 <Badge
                   showZero
